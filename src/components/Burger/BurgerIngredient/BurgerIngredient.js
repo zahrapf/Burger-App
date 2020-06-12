005e1b72fac503ca7c31b883ecb12cf5e0b07fc5
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-06-12 09:50:48 -0400

    Add the BurgerIngredient to BurgerBuilder

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import './burgerIngredient.css';
+import './BurgerIngredient.css';
 
 class BurgerIngredient extends Component {
     render () {
