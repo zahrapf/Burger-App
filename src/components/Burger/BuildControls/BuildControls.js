6b94b75d7b3c3b3754e71f43b9e2e8d65021490b
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-06-12 14:10:17 -0400

    Add BuildControls component for adding ingredients dynamically

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import './BuildControls.css'
 
-const buildControls = (props => {
-    <div>
-        
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'}
+];
+
+const buildControls = (props) => (
+    <div className="BuildControls">
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
-});
+);
 
 export default buildControls;
