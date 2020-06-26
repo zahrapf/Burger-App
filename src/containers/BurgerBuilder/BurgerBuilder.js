39affc4c24124b523a51ae33a61acc204dd5d0a6
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-06-25 20:57:38 -0400

    Implement post request to send the orders to firebase dbs

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
+import https from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,7 +77,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+       // alert('You continue!');
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: 'Zara',
+               address: {
+                   street: 'Young st.',
+                   postal: 'm2y5r6',
+                   country: 'CA'
+               },
+               email: 'test@test.com'
+           },
+           deliveryMethod: 'fastest'
+       }
+        https.post('/orders.json', order)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => console.log(error));
     }
 
     render() {
