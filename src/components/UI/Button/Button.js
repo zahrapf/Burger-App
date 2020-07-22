b9186ae4db21835e8ef306e5bb2adfb1d8de0142
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-07-21 20:12:46 -0400

    Pass ingredients using query params

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
 const button = (props) => (
-    <button className={'Button', [props.btnType].join(' ')}
-        onClick={props.clicked}>
-        {props.children}
-    </button>
+  <button
+    className={("Button", [props.btnType].join(" "))}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
 );
 export default button;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -113,7 +113,19 @@ class BurgerBuilder extends Component {
     //             this.setState({loading: true, purchasing: false});
     //             console.log(error)
     //         });
-    this.props.history.push("/checkout");
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,
+    });
   };
 
   render() {
